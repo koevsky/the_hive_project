5bb17ee718e4c9566e702248776c5ea3a51b73a0
Kaloyan Koev <kkoev9601@gmail.com>
2023-07-04 18:27:05 +0300

    Configured media files

--- the_hive_project/settings.py
+++ the_hive_project/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +118,7 @@ STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 MEDIA_URL = '/media/'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- the_hive_project/urls.py
+++ the_hive_project/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('custom-admin/', include('custom_admin_app.urls')),
     path('accounts/', include('accounts_app.urls')),
     path('', include('hive_app.urls')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
