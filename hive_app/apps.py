3b701050731131c2a0eca30243b52601f71cc218
Kaloyan Koev <kkoev9601@gmail.com>
2023-07-04 17:50:40 +0300

    Created main apps, configured urls, staticfiles and db

---
+++ hive_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HiveAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hive_app'
--- the_hive_project/urls.py
+++ the_hive_project/urls.py
@@ -1,7 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('custom-admin/', include('custom_admin_app.urls')),
+    path('accounts/', include('accounts_app.urls')),
+    path('', include('hive_app.urls')),
 ]
